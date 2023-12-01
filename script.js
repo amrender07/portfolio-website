b288950412a8eb72989ad14f1ed419846db242ef
AMRENDER SINGH <118029886+amrender07@users.noreply.github.com>
2023-12-01 16:26:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+const texts = ["Web Developer", "Data Analyst"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.section__text .section__text__p2').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type, 180);
+}
+
+type();
+
+
+/*dark mode ki js */
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
